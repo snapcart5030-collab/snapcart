c76c1f13cf9193be3a76737c95f3bb68a2f8e1c1
ganesh-sherkar <sherkarg527@gmail.com>
2025-10-27 16:19:32 +0530

    Updated server configuration

--- models/Category.js
+++ models/Category.js
@@ -7,21 +7,20 @@ const ProductSchema = new mongoose.Schema({
   productPrice: { type: Number, required: true },
   productDisc: String,
   productImage: String,
-}, { _id: true });
+}, { _id: true }); // auto _id for each product
 
-// Subcategory schema
+// Subcategory schema (e.g. Mango, Banana)
 const SubcategorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
   products: [ProductSchema]
 }, { _id: true });
 
-// Category schema
+// Category schema (e.g. Fruits)
 const CategorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: String,
   subcategories: [SubcategorySchema]
 }, { timestamps: true });
 
-// 👇 इथे collection नाव स्पष्ट दिलंय
-module.exports = mongoose.model('Category', CategorySchema, 'categories');
+module.exports = mongoose.model('Category', CategorySchema);
